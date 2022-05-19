b9148bcd61e1fa54bfb36747d6f33f45eb4b5883
Raqsf <raquelsf@ua.pt>
2022-05-19 17:31:46 +0100

    update java version

---
+++ projDelivera/backend/delivera/src/main/java/ua/tqs/delivera/DeliveraApplication.java
@@ -0,0 +1,13 @@
+package ua.tqs.delivera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveraApplication.class, args);
+	}
+
+}
---
+++ projFrostini/backend/frostini/src/main/java/ua/tqs/frostini/FrostiniApplication.java
@@ -0,0 +1,13 @@
+package ua.tqs.frostini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrostiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrostiniApplication.class, args);
+	}
+
+}
