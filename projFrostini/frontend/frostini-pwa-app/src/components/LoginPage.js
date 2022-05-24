78dcdb3e0c8ec87df7818aad5c46b1b65f97da00
sophjane <sophiepousinho@gmail.com>
2022-05-24 20:38:06 +0100

    frostini web app init

---
+++ projFrostini/frontend/frostini-pwa-app/src/components/LoginPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./LoginPage.css";
+
+function LoginPage() {
+    return
+}
+
+export default LoginPage;
---
+++ projFrostini/frontend/frostini-pwa-app/src/components/OrderPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./OrderPage.css";
+
+function OrderPage() {
+    return (
+        <div>
+            <h1>Order Page</h1>
+        </div>
+    );
+}
+
+export default OrderPage;
