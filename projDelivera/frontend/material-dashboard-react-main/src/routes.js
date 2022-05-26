c24d8b4d201ffe3fdf8259464b9b313830ad04cd
Patricia <pmd8@ua.pt>
2022-05-26 11:09:01 +0100

    list of products, delivera client

--- projDelivera/frontend/material-dashboard-react-main/src/routes.js
+++ projDelivera/frontend/material-dashboard-react-main/src/routes.js
@@ -44,6 +44,7 @@ import Notifications from "layouts/notifications";
 import Profile from "layouts/profile";
 import SignIn from "layouts/authentication/sign-in";
 import SignUp from "layouts/authentication/sign-up";
+import Products from "layouts/products";
 
 // @mui icons
 import Icon from "@mui/material/Icon";
@@ -56,6 +57,14 @@ const routes = [
     icon: <Icon fontSize="small">dashboard</Icon>,
     route: "/dashboard",
     component: <Dashboard />,
+  },
+  {
+    type: "collapse",
+    name: "Products",
+    key: "products",
+    icon: <Icon fontSize="small">inventory</Icon>,
+    route: "/products",
+    component: <Products />,
   },
   {
     type: "collapse",
