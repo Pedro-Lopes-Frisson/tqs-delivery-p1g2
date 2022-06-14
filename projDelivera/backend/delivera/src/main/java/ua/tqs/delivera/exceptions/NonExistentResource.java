cac8a212a649232b7bf5f7ed9d221adb001a154a
Raqsf <raquelsf@ua.pt>
2022-06-14 01:02:58 +0100

    Some riders statistics

---
+++ projDelivera/backend/delivera/src/main/java/ua/tqs/delivera/exceptions/NonExistentResource.java
@@ -0,0 +1,9 @@
+package ua.tqs.delivera.exceptions;
+
+public class NonExistentResource extends Exception {
+
+    public NonExistentResource(String message) {
+        super(message);
+    }
+    
+}
