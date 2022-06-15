29b282bcfd6766aa396577ade0d7ba1f121f59e3
Pedro <pdfl@ua.pt>
2022-06-15 16:17:52 +0100

    Add merger

--- projDelivera/frontend/material-dashboard-react-main/src/index.js
+++ projDelivera/frontend/material-dashboard-react-main/src/index.js
@@ -17,9 +17,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "App";
-
 // Material Dashboard 2 React Context Provider
 import { MaterialUIControllerProvider } from "context";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(
   <BrowserRouter>
@@ -29,3 +29,10 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+
+console.log("REGISTER OWRKSE");
+serviceWorkerRegistration.register();
