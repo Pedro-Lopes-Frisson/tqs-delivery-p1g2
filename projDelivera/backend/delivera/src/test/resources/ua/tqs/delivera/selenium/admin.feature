fa7616f68109ba347cb1521763c965ff8eed3a87
Pedro <pdfl@ua.pt>
2022-06-16 01:47:57 +0100

    Fix paths

--- projDelivera/backend/delivera/src/test/resources/ua/tqs/delivera/selenium/admin.feature
+++ projDelivera/backend/delivera/src/test/resources/ua/tqs/delivera/selenium/admin.feature
@@ -1,7 +1,7 @@
 Feature: Manage the statistics from Delivera
 
     Scenario: Admin signs in successfully
-        When I access "http://localhost:3000"
+        When I access "http://localhost:3000/authentication/sign-in"
         Then I fill in the sign in with the necessary information
         And I click Sign In
         And I should be redirected to the "Dashboard" page
--- projDelivera/frontend/material-dashboard-react-main/src/App.js
+++ projDelivera/frontend/material-dashboard-react-main/src/App.js
@@ -2,14 +2,10 @@
 =========================================================
 * Material Dashboard 2 React - v2.1.0
 =========================================================
-
 * Product Page: https://www.creative-tim.com/product/material-dashboard-react
 * Copyright 2022 Creative Tim (https://www.creative-tim.com)
-
 Coded by www.creative-tim.com
-
  =========================================================
-
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
@@ -169,7 +165,7 @@ export default function App() {
         {layout === "vr" && <Configurator />}
         <Routes>
           {getRoutes(routes)}
-          <Route path="*" element={<Navigate to="/authentication/sign-in" />} />
+          <Route path="*" element={<Navigate to="/dashboard" />} />
         </Routes>
       </ThemeProvider>
     </CacheProvider>
