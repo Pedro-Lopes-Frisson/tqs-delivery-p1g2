36b4f69cb65bff11cc11fbc67f8b4a2d49fc5f86
Patricia <pmd8@ua.pt>
2022-06-23 08:53:05 +0100

    added controller for order status

--- projDelivera/backend/delivera/src/main/java/ua/tqs/delivera/controllers/OrdersController.java
+++ projDelivera/backend/delivera/src/main/java/ua/tqs/delivera/controllers/OrdersController.java
@@ -2,6 +2,7 @@ package ua.tqs.delivera.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,11 @@ import ua.tqs.delivera.models.Order;
 public class OrdersController {
     @PostMapping
     public ResponseEntity<Order> createOrder( @RequestBody Order orderDTO){
+        return null;
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<String> updateOrderStatus(@PathVariable Long id){
         return null;
     }
 }
